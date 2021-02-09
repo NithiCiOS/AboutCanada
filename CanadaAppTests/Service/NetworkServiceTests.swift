8a5a6db19bebd7dc1df9aa45a472074d5b7e4fc0
CVN <>
2021-02-09 11:24:07 +0530

    CodeFreez: Docs of NetworkServiceTests has been added.

--- CanadaAppTests/Service/NetworkServiceTests.swift
+++ CanadaAppTests/Service/NetworkServiceTests.swift
@@ -8,33 +8,45 @@
 import XCTest
 @testable import CanadaApp
 
+/**
+ * The intention of this tests is to verify the actual `NetworkService` executions.
+ */
 class NetworkServiceTests: XCTestCase {
+    // MARK: - Intance Variables
     
     /// System-Under-Test
     var sutNetworkService: NetworkService?
     
+    /// `MockNetworkSession` respresents the mock session.
     let mockSession =  MockNetworkSession()
     
+    // MARK: - Lifecycle
+    
+    /// Call everytime before start the testcase.
     override func setUp() {
         sutNetworkService = NetworkService(session: mockSession)
     }
     
+    /// Call everytime when testcase complete its process.
     override func tearDown() {
         sutNetworkService = nil
     }
     
+    // MARK: - Test cases.
+    
+    /// Verifies the `loadData(from:completion:)` execution.
     func testLoadData() {
         // Arrange
         
         // Act
         sutNetworkService?.getAboutCanada(completion: { result in
         })
-        
-        // Assert
+
         XCTAssertTrue(mockSession.loadDataCalled)
         XCTAssertEqual(mockSession.loadDataCallCount, 1)
     }
     
+    /// Verifies the `loadImageData(from:completion:)` execution.
     func testLoadImageData() {
         // Arrange
         
